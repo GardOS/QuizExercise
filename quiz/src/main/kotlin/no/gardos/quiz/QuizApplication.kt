ed6ce22f8b1cdaab80092fdf57fab5c9b46d793d
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-03-11 17:28:39 +0100

    Fix that api-documentation exposes too much info

--- quiz/src/main/kotlin/no/gardos/quiz/QuizApplication.kt
+++ quiz/src/main/kotlin/no/gardos/quiz/QuizApplication.kt
@@ -6,47 +6,48 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
-@EntityScan
 @EnableSwagger2
-class QuizApplication
+class QuizApplication {
+	@Bean
+	fun swaggerApi(): Docket {
+		return Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("no.gardos.quiz"))
+				.build()
+	}
 
-@Bean
-fun swaggerApi(): Docket {
-	return Docket(DocumentationType.SWAGGER_2)
-			.apiInfo(apiInfo())
-			.select()
-			.paths(PathSelectors.any())
-			.build()
+	private fun apiInfo(): ApiInfo {
+		return ApiInfoBuilder()
+				.title("API for REST")
+				.description("Some description")
+				.version("1.0")
+				.build()
+	}
+
+	@Bean(name = ["OBJECT_MAPPER_BEAN"])
+	fun jsonObjectMapper(): ObjectMapper {
+		return Jackson2ObjectMapperBuilder.json()
+				.serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values?
+				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+				.modules(JavaTimeModule())
+				.build()
+	}
 }
 
-private fun apiInfo(): ApiInfo {
-	return ApiInfoBuilder()
-			.title("API for REST")
-			.description("Some description")
-			.version("1.0")
-			.build()
-}
-
-@Bean(name = ["OBJECT_MAPPER_BEAN"])
-fun jsonObjectMapper(): ObjectMapper {
-	return Jackson2ObjectMapperBuilder.json()
-			.serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values?
-			.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-			.modules(JavaTimeModule())
-			.build()
-}
-
+//mvn spring-boot:run
 //http://localhost:8080/quizrest/api/swagger-ui.html
 fun main(args: Array<String>) {
 	SpringApplication.run(QuizApplication::class.java, *args)
