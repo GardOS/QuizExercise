62f7c0fb5ad01ce551e3c9a1c43bcbb58ab7a9ea
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-03-27 15:54:33 +0200

    Add tests for creating questions

--- quiz/src/test/kotlin/no/gardos/quiz/QuestionApiTest.kt
+++ quiz/src/test/kotlin/no/gardos/quiz/QuestionApiTest.kt
@@ -1,24 +1,200 @@
 package no.gardos.quiz
 
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.equalTo
+import io.restassured.http.ContentType
+import no.gardos.quiz.model.dto.QuestionDto
 import org.junit.Test
 
 class QuestionApiTest : ApiTestBase() {
 
 	@Test
-	fun createQuestion_ValidQuestion_QuestionCreated() {
-		given().get(QUESTION_PATH)
-				.then()
-				.statusCode(200)
-				.body("size()", equalTo(0))
-
+	fun getQuizzes_ValidQuizzes_QuizzesReturned() {
 		val category = createGenericCategory("Category")
 		createGenericQuestion(category)
+		createGenericQuestion(category)
 
-		given().get(QUESTION_PATH)
+		val response = given().get(QUESTION_PATH)
 				.then()
 				.statusCode(200)
-				.body("size()", equalTo(1))
+				.extract()
+				.`as`(Array<QuestionDto>::class.java)
+				.toList()
+
+		assert(response.count() == 2)
+	}
+
+	@Test
+	fun createQuestion_ValidQuestion_Created() {
+		val category = createGenericCategory("Category")
+		createGenericQuestion(category)
+	}
+
+	@Test
+	fun createQuestion_IdSpecified_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				id = 1234,
+				questionText = "What is 1+1?",
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = 2,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_NullQuestionText_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				questionText = null,
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = 2,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_EmptyQuestionText_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				questionText = "",
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = 2,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_TooFewAnswers_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				questionText = "What is 1+1?",
+				answers = listOf("2"),
+				correctAnswer = 0,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_TooManyAnswers_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				questionText = "What is 1+1?",
+				answers = listOf("0", "1", "2", "3", "4"),
+				correctAnswer = 0,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_NullCorrectAnswer_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				questionText = "What is 1+1?",
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = null,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_TooSmallCorrectAnswer_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				questionText = "What is 1+1?",
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = -1,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_TooBigCorrectAnswer_BadRequest() {
+		val category = createGenericCategory("Category")
+		val question = QuestionDto(
+				questionText = "What is 1+1?",
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = 4,
+				category = category
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
+	}
+
+	@Test
+	fun createQuestion_NullCategory_Created() {
+		val question = QuestionDto(
+				questionText = "What is 1+1?",
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = 2,
+				category = null
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(201)
+	}
+
+	@Test
+	fun createQuestion_CategoryDoNotExist_BadRequest() {
+		val question = QuestionDto(
+				questionText = "What is 1+1?",
+				answers = listOf("0", "1", "2", "3"),
+				correctAnswer = 2,
+				category = 1234
+		)
+
+		given().contentType(ContentType.JSON)
+				.body(question)
+				.post(QUESTION_PATH)
+				.then()
+				.statusCode(400)
 	}
 }
