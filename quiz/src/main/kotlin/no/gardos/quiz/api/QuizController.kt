94978cfff332e1dd817d79e9ca0e86f3bf1c3660
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-04-15 13:49:31 +0200

    Add generic api-method for quiz so that something is exposed in swagger

--- quiz/src/main/kotlin/no/gardos/quiz/api/QuizController.kt
+++ quiz/src/main/kotlin/no/gardos/quiz/api/QuizController.kt
@@ -1,10 +1,16 @@
 package no.gardos.quiz.api
 
 import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import io.swagger.annotations.ApiResponse
+import no.gardos.quiz.model.dto.QuizDto
+import no.gardos.quiz.model.entity.Quiz
 import no.gardos.quiz.model.repository.QuizRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import javax.validation.ConstraintViolationException
@@ -19,6 +25,24 @@ import javax.validation.ConstraintViolationException
 class QuizController {
 	@Autowired
 	private lateinit var quizRepo: QuizRepository
+
+	@ApiOperation("Create new quiz")
+	@PostMapping(consumes = [(MediaType.APPLICATION_JSON_VALUE)])
+	@ApiResponse(code = 201, message = "The id of newly created quiz")
+	fun createQuiz(
+			@ApiParam("Quiz name. Should not specify id")
+			@RequestBody
+			dto: QuizDto
+	): ResponseEntity<Long> {
+		//Id is auto-generated and should not be specified
+		if (dto.id != null) {
+			return ResponseEntity.status(400).build()
+		}
+
+		val quiz = quizRepo.save(Quiz())
+
+		return ResponseEntity.status(201).body(quiz.id)
+	}
 
 	//Catches validation errors and returns 400 instead of 500
 	@ExceptionHandler(value = [(ConstraintViolationException::class)])
