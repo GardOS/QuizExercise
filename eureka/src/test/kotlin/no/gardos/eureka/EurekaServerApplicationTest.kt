62a2f48303889241af5179e7a3be8693cfa5dd3c
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-04-15 14:39:00 +0200

    Add module for Eureka server

---
+++ eureka/src/test/kotlin/no/gardos/eureka/EurekaServerApplicationTest.kt
@@ -0,0 +1,15 @@
+package no.gardos.eureka
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
