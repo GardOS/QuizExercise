f6fda8e26671651862c221a7d71527526ef24cf2
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-04-29 13:12:24 +0200

    Move quiz application configuration to own class

--- quiz/src/main/kotlin/no/gardos/quiz/QuizApplication.kt
+++ quiz/src/main/kotlin/no/gardos/quiz/QuizApplication.kt
@@ -1,63 +1,15 @@
 package no.gardos.quiz
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.context.annotation.Bean
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import org.springframework.web.client.RestTemplate
-import springfox.documentation.builders.ApiInfoBuilder
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.service.ApiInfo
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableSwagger2
-class QuizApplication {
-	@Bean
-	fun swaggerApi(): Docket {
-		return Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo())
-				.select()
-				.paths(PathSelectors.any())
-				.apis(RequestHandlerSelectors.basePackage("no.gardos.quiz"))
-				.build()
-	}
-
-	private fun apiInfo(): ApiInfo {
-		return ApiInfoBuilder()
-				.title("REST API for managing quizzes")
-				.version("1.0")
-				.build()
-	}
-
-	@Bean(name = ["OBJECT_MAPPER_BEAN"])
-	fun jsonObjectMapper(): ObjectMapper {
-		return Jackson2ObjectMapperBuilder.json()
-				.serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values?
-				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-				.modules(JavaTimeModule())
-				.build()
-	}
-
-	@LoadBalanced
-	@Bean
-	fun restTemplate(): RestTemplate {
-		return RestTemplate()
-	}
-}
+class QuizApplication
 
 //docker-compose down && mvn package -DskipTests && docker-compose build && docker-compose up -d
-//http://localhost:8080/quizexercise/api/swagger-ui.html
+//http://localhost:8080/swagger-ui.html
 fun main(args: Array<String>) {
 	SpringApplication.run(QuizApplication::class.java, *args)
 }
