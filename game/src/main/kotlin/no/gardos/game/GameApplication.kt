ca50217006aeeafbe967bd433d7470fd7bf3dba5
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-04-29 13:11:16 +0200

    Add application for game

--- game/src/main/kotlin/no/gardos/game/GameApplication.kt
+++ game/src/main/kotlin/no/gardos/game/GameApplication.kt
@@ -1,2 +1,17 @@
 package no.gardos.game
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.ribbon.RibbonClient
+
+@SpringBootApplication
+@EnableEurekaClient
+@RibbonClient(name = "quiz-server")
+class GameApplication
+
+//docker-compose down && mvn package -DskipTests && docker-compose build && docker-compose up -d
+//http://localhost:8080/swagger-ui.html
+fun main(args: Array<String>) {
+	SpringApplication.run(GameApplication::class.java, *args)
+}
