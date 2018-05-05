657b1eff79374b5a4f7480207276f0f19ac4a7d5
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-05-05 17:50:41 +0200

    Rename application names of modules

--- gateway/src/main/kotlin/no/gardos/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/no/gardos/gateway/GatewayApplication.kt
@@ -2,14 +2,15 @@ package no.gardos.gateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
-@EnableZuulProxy //Todo: Investigate spring cloud gateway
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableZuulProxy //Todo: Investigate spring cloud gateway
 class GatewayApplication
 
+//docker-compose down && mvn package -DskipTests && docker-compose build && docker-compose up -d
 fun main(args: Array<String>) {
 	SpringApplication.run(GatewayApplication::class.java, *args)
 }
