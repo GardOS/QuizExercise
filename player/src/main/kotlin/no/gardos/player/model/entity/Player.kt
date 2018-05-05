14eed11c6f96ea7c4f9245614120dc234643a33b
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-05-05 17:51:27 +0200

    Add field for Player entity so that hibernate gets context

--- player/src/main/kotlin/no/gardos/player/model/entity/Player.kt
+++ player/src/main/kotlin/no/gardos/player/model/entity/Player.kt
@@ -1,6 +1,11 @@
 package no.gardos.player.model.entity
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
 
 @Entity
-class Player
+class Player(
+		@get:Id @get:GeneratedValue
+		var id: Long? = null
+)
