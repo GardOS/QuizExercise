34a30b5a782d8c491cc61063f1630b7e96ba3f82
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-05-12 13:48:46 +0200

    Add swagger-ui dependency

--- quiz/src/main/kotlin/no/gardos/quiz/QuizApplicationConfig.kt
+++ quiz/src/main/kotlin/no/gardos/quiz/QuizApplicationConfig.kt
@@ -20,7 +20,7 @@ import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2 //TODO: Investigate 406 error on swagger ui
 @EnableHystrixDashboard
 class QuizApplicationConfig {
 
