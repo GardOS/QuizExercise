68d44628f0fff27ca504dd4ca1afb3048bbed2d9
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-05-13 16:26:51 +0200

    Remove finished //TODO:

--- quiz/src/main/kotlin/no/gardos/quiz/QuizApplicationConfig.kt
+++ quiz/src/main/kotlin/no/gardos/quiz/QuizApplicationConfig.kt
@@ -20,7 +20,7 @@ import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
-@EnableSwagger2 //TODO: Investigate 406 error on swagger ui
+@EnableSwagger2
 @EnableHystrixDashboard
 class QuizApplicationConfig {
 
