d80505f6013b994bae8bacd009247657645f97e5
Gard Opsahl Skaare <Gardo1506@gmail.com>
2018-05-19 12:01:49 +0200

    Add admin user on boot

--- gateway/src/main/kotlin/no/gardos/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/no/gardos/gateway/GatewayApplication.kt
@@ -1,5 +1,9 @@
 package no.gardos.gateway
 
+import no.gardos.gateway.model.User
+import no.gardos.gateway.model.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
@@ -7,6 +11,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
 import springfox.documentation.swagger.web.UiConfiguration
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
@@ -29,4 +34,17 @@ class GatewayApplication {
 
 fun main(args: Array<String>) {
 	SpringApplication.run(GatewayApplication::class.java, *args)
+}
+
+@Component
+internal class CommandLineAppStartupRunner : CommandLineRunner {
+	@Autowired
+	var userRepository: UserRepository? = null
+	@Autowired
+	var passwordEncoder: PasswordEncoder? = null
+
+	override fun run(vararg args: String) {
+		val roles = hashSetOf("ROLE_USER", "ROLE_ADMIN")
+		userRepository!!.save(User("admin", passwordEncoder!!.encode("pwd"), roles))
+	}
 }
